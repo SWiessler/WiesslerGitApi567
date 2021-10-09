84497f0dc25119a26aa14bb96c36223406898c96
SWiessler <s.wiessler@comcast.net>
2021-10-09 12:55:07 -0400

    Implementing Mock Testing

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # WiesslerGitApi567
 
-[![Build Status](https://app.travis-ci.com/SWiessler/WiesslerGitApi567.svg?branch=main)](https://app.travis-ci.com/SWiessler/WiesslerGitApi567)
+# HW05_Mocking  Branch Badge
+[![Build Status](https://app.travis-ci.com/SWiessler/WiesslerGitApi567.svg?branch=HW05_Mocking)](https://app.travis-ci.com/SWiessler/WiesslerGitApi567)
--- TestMain.py
+++ TestMain.py
@@ -1,11 +1,14 @@
-import unittest
+# following https://realpython.com/testing-third-party-apis-with-mocks/#first-steps
+from unittest.mock import Mock, patch
+# Third-party imports...
+from nose.tools import assert_is_not_none
 
 from Main import getRepoandCommits
 
-class TestMain(unittest.TestCase):
-    def testgetRepo(self): 
-        self.assertEqual(getRepoandCommits("SWiessler"),['Repo: CS-546 Number of commits: 30', 'Repo: Complexity Number of commits: 30', 'Repo: SSW-423 Number of commits: 1', 'Repo: SSW345 Number of commits: 3', 'Repo: WiesslerGitApi567 Number of commits: 4', 'Repo: express-api Number of commits: 2', 'Repo: ssw423_hw1 Number of commits: 3', 'Repo: wiessler_triangle Number of commits: 12'])
-   
-if __name__ == '__main__':
-    print('Running unit tests')
-    unittest.main()
+@patch('Main.requests.get')
+def testgetRepo(mock_get): 
+    mock_get.return_value.ok = True
+    response = getRepoandCommits("SWiessler")
+    assert_is_not_none(response)
+    # ['Repo: CS-546 Number of commits: 30', 'Repo: Complexity Number of commits: 30', 'Repo: SSW-423 Number of commits: 1', 'Repo: SSW345 Number of commits: 3', 'Repo: WiesslerGitApi567 Number of commits: 4', 'Repo: express-api Number of commits: 2', 'Repo: ssw423_hw1 Number of commits: 3', 'Repo: wiessler_triangle Number of commits: 12']
+
